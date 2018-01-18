0c5c875f0b8aedca03c25a3cc2226888a10f9194
Igor Grafin <apaticmail@gmail.com>
2018-01-18 22:36:09 +0300

    removed exceptions, renamed variables

--- pprint_json.py
+++ pprint_json.py
@@ -3,9 +3,16 @@ import sys
 
 
 def load_data(file_path):
-    with open(file_path, "r") as file:
-        json_data = json.load(file)
-        return json_data
+    try:
+        with open(file_path, "r") as file:
+            json_data = json.load(file)
+            return json_data
+    except FileNotFoundError:
+        print("File %s not found!" % file_name)
+        sys.exit(0)
+    except json.decoder.JSONDecodeError:
+        print("JSON is not valid!")
+        sys.exit(0)
 
 
 def pretty_print_json(json_data):
