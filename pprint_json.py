afbeba0f9080c68f2c0aa824847753e581f2860c
Igor Grafin <apaticmail@gmail.com>
2018-01-18 23:10:45 +0300

    fix in exceptions in functions

--- pprint_json.py
+++ pprint_json.py
@@ -9,10 +9,10 @@ def load_data(file_path):
             return json_data
     except FileNotFoundError:
         print("File %s not found!" % file_name)
-        sys.exit(0)
+        return None
     except json.decoder.JSONDecodeError:
         print("JSON is not valid!")
-        sys.exit(0)
+        return None
 
 
 def pretty_print_json(json_data):
@@ -23,6 +23,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         file_name = sys.argv[1]
         input_data = load_data(file_name)
-        pretty_print_json(input_data)
+        if input_data:
+            pretty_print_json(input_data)
     else:
         print("Please, put a file name as a parameter. For example: 'python pprint_json.py in.json' ")
