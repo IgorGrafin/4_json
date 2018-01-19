d77b992b1e9e3fb07d53d92524248a9a4dce47c5
Igor Grafin <apaticmail@gmail.com>
2018-01-19 23:28:17 +0300

    main function added. exceptions refactor

--- pprint_json.py
+++ pprint_json.py
@@ -3,28 +3,28 @@ import sys
 
 
 def load_data(file_path):
-    try:
-        with open(file_path, "r") as file:
-            json_data = json.load(file)
-            return json_data
-    except (FileNotFoundError, json.decoder.JSONDecodeError) as err:
-        return err
+    with open(file_path, "r") as file:
+        json_data = json.load(file)
+        return json_data
 
 
 def pretty_print_json(json_data):
     print(json.dumps(json_data, ensure_ascii=False, sort_keys=True, indent=4))
 
 
+def main(argv):
+    file_name = sys.argv[1]
+    input_data = load_data(file_name)
+    pretty_print_json(input_data)
+
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        file_name = sys.argv[1]
-        input_data = load_data(file_name)
-        print(type(input_data))
-        if type(input_data) == dict:
-            pretty_print_json(input_data)
-        elif type(input_data) == FileNotFoundError:
+        try:
+            main(sys.argv[1])
+        except FileNotFoundError:
             print("File %s not found" % sys.argv[1])
-        elif type(input_data) == json.decoder.JSONDecodeError:
-            print('JSON is not valid')
+        except json.decoder.JSONDecodeError:
+            print("JSON is not valid")
     else:
         print("Please, put a file name as a parameter. For example: 'python pprint_json.py in.json' ")
