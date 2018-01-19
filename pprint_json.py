5f5e202da03501cde68d2e6ddf7a82dc719b8662
Igor Grafin <apaticmail@gmail.com>
2018-01-20 00:20:55 +0300

    main function removed. exception added

--- pprint_json.py
+++ pprint_json.py
@@ -12,19 +12,14 @@ def pretty_print_json(json_data):
     print(json.dumps(json_data, ensure_ascii=False, sort_keys=True, indent=4))
 
 
-def main(argv):
-    file_name = sys.argv[1]
-    input_data = load_data(file_name)
-    pretty_print_json(input_data)
-
-
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        try:
-            main(sys.argv[1])
-        except FileNotFoundError:
-            print("File %s not found" % sys.argv[1])
-        except json.decoder.JSONDecodeError:
-            print("JSON is not valid")
-    else:
+    try:
+        file_name = sys.argv[1]
+        input_data = load_data(file_name)
+        pretty_print_json(input_data)
+    except FileNotFoundError:
+        print("File %s not found" % sys.argv[1])
+    except json.decoder.JSONDecodeError:
+        print("JSON is not valid")
+    except IndexError:
         print("Please, put a file name as a parameter. For example: 'python pprint_json.py in.json' ")
